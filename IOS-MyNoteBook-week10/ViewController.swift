654b6981fe5ec099667bca3e7dddf85ba3658381
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-06-24 16:27:59 +0200

    Initial Commit

---
+++ IOS-MyNoteBook-week10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-MyNoteBook-week10
+//
+//  Created by Rasmus Knoth Nielsen on 24/06/2020.
+//  Copyright © 2020 Rasmus Knoth Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
